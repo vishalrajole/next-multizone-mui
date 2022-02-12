17473a8ea9c3f255f5c9c72548260a74ee39dffa
vishalrajole <vishalrajole1991@gmail.com>
2022-02-12 08:49:59 +0100

    inital zone setup with mui

--- home/components/Header.js
+++ home/components/Header.js
@@ -1,7 +1,10 @@
+import Button from "@mui/material/Button";
+
 const Header = () => (
   <div>
     <h2>The Company</h2>
+    <Button variant="contained">Hello World</Button>
   </div>
-)
+);
 
-export default Header
+export default Header;
